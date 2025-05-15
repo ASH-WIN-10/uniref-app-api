e6032aafd7f852c9d5f3f5e403af72b08417d56d
Ashvin Jangid <ashvinjangid10@gmail.com>
2025-05-15 12:43:45 +0530

    setup connection pool with a postgres database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"os"
+	"time"
 
 	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +18,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
@@ -29,7 +36,17 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 8080, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("HIMWAN_DB_DSN"), "PostgreSQL DSN")
 	flag.Parse()
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	logger.Info("Database connection pool established")
 
 	app := &application{
 		config: cfg,
@@ -40,4 +57,22 @@ func main() {
 
 	logger.Info("Starting server", "version", version, "port", cfg.port, "env", cfg.env)
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", cfg.port)))
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
