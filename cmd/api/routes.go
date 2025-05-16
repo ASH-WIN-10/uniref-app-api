1c5d04251fb07f5ff158be5791003e7341be0cd0
Ashvin Jangid <ashvinjangid10@gmail.com>
2025-05-16 18:27:05 +0530

    setup cors middleware and change static url

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func (app *application) registerRoutes(e *echo.Echo) {
 	e.HTTPErrorHandler = app.customHTTPErrorHandler
 
 	e.Use(app.recoverPanic)
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"http://localhost:1420", "tauri://localhost"},
+		AllowMethods:     []string{echo.GET, echo.POST, echo.PUT, echo.DELETE, echo.OPTIONS},
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+		AllowCredentials: true,
+	}))
 
-	e.Static("/static", "static")
+	e.Static("/assets", "assets")
 
 	e.GET("/v1/healthcheck", app.healthcheckHandler)
 	e.POST("/v1/clients", app.createClientHandler)
